ba9885dfdfdec92fe62e8385848deff24819a1b9
Clint Bryan Nguena <clint.bryan.nguena@mni.thm.de>
2026-01-15 04:50:54 +0100

    fix alle fehlgeschlagene Implementierung im Backend entfernen(AuthController): create a road to load all users

--- frontend/src/utils/getIcon.js
+++
@@ -1,7 +0,0 @@
-import mdiIcons from '@/assets/icons/mdi-icons.json';
-
-const iconMap = Object.fromEntries(mdiIcons.map(i => [i.name, i.path]));
-
-export const getIcon = (name) => {
-    return iconMap[name] || '';
-};
--- frontend/src/utils/getUser.ts
+++
@@ -1,9 +0,0 @@
-const user = JSON.parse(localStorage.getItem('user'))?? null
-
-export const getCurentUserId = () => {
-    return user.id;
-};
-
-export const getCurrentUserInfos = () => {
-    return user.user_metadata;
-};
